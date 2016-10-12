5cf75736e90795fdd40c62d38fddde7b3b2c49b9
Jeff Duke <jeff.duke@gmail.com>
2016-10-12 16:49:15 -0600

    fix notes

--- app/models/reminder.js
+++ app/models/reminder.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  date: DS.attr('date')
+  date: DS.attr('date'),
+  notes: DS.attr('string'),
+  pinned: DS.attr('boolean')
 });
