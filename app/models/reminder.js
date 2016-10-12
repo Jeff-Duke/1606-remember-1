3a3094d6efb6f574cb05d88170e1508555e61a6c
Jeff Duke <jeff.duke@gmail.com>
2016-10-12 15:20:00 -0600

    passing first test in acceptance test

---
+++ app/models/reminder.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  date: DS.attr('date'),
+  notes: DS.hasMany('note'),
+  pinned: DS.attr('boolean')
+});
