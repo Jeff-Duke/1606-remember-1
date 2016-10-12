8c922e590815e2b58f6fcd76ffb0687466339fd8
Jeff Duke <jeff.duke@gmail.com>
2016-10-12 16:47:20 -0600

    remove notes relationship and pinned from reminder model

--- app/models/reminder.js
+++ app/models/reminder.js
@@ -2,7 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
-  date: DS.attr('date'),
-  notes: DS.hasMany('note'),
-  pinned: DS.attr('boolean')
+  date: DS.attr('date')
 });
