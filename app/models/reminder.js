8839046a1da524f12879f2757ee5bd6b599bf520
Jeff Duke <jeff.duke@gmail.com>
2016-10-13 09:31:14 -0600

    implement feature 2, install sass, really basic styles

--- app/models/reminder.js
+++ app/models/reminder.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   date: DS.attr('date'),
-  notes: DS.hasMany('note'),
-  pinned: DS.attr('boolean', { defaultValue: false }),
+  notes: DS.attr('string'),
+  pinned: DS.attr('boolean', {
+    defaultValue: false
+  }),
 });
