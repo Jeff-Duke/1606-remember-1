6bd82c84f6db0768f8a41d733e92d1abc366ea06
David Kerr <kerrd89@gmail.com>
2016-10-13 16:18:05 -0600

    refactor form and actions to component

--- app/routes/reminders/new.js
+++ app/routes/reminders/new.js
@@ -4,14 +4,6 @@ export default Ember.Route.extend({
   model() {
     return Ember.Object.create();
   },
+
   
-  actions: {
-    createReminder(reminderForm) {
-      let reminder = reminderForm.getProperties('title', 'date', 'notes');
-      reminder.date = new Date(reminder.date);
-      this.get('store').createRecord('reminder', reminder).save().then(() => {
-        this.setProperties({ title: '', date: '', notes: '' });
-      });
-    }
-  }
 });
