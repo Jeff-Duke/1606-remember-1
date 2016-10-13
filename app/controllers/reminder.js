f1f5adf112b69988ca01b1b111e995daecb774b3
David Kerr <kerrd89@gmail.com>
2016-10-13 14:54:35 -0600

    bring on the conflicts

---
+++ app/controllers/reminder.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    editTodo: function() {
+      this.set('isEditing', true);
+    }
+  },
+
+  isEditing: false
+});
