66d6a6965364e2dc9a1f1fcfa56f239972c3e366
David Kerr <kerrd89@gmail.com>
2016-10-13 15:36:38 -0600

    remove console log

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -6,7 +6,6 @@ export default Ember.Controller.extend({
     createReminder(reminderForm) {
       let reminder = reminderForm.getProperties('title', 'date', 'notes');
       reminder.date = new Date(reminder.date);
-      console.log(reminder)
       this.get('store').createRecord('reminder', reminder).save().then(() => {
         this.setProperties({ title: '', date: '', notes: '' });
       });
