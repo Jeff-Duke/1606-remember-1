d94e7e2a9b124e820ecfd1738a458675992353ef
Jeff Duke <jeff.duke@gmail.com>
2016-10-13 08:56:59 -0600

    pass all tests - apply styling

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('reminder', {path: ':reminder_id'});
+  this.route('reminders', { path: '/' }, function() {
+    this.route('reminder', { path: ':reminder_id'});
+  });
 });
 
 export default Router;
