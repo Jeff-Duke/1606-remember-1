2aa970b4d61e1b70a5d7cb4667dc3b84c1fea227
David Kerr <kerrd89@gmail.com>
2016-10-13 13:53:25 -0600

    add form component with testing

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -34,3 +34,26 @@ test('clicking on an individual item', function(assert) {
     assert.equal(Ember.$('.spec-reminder-item:first').text().trim(), Ember.$('.spec-reminder-title').text().trim());
   });
 });
+
+test('clicking on new and creating a new item', function(assert) {
+
+  visit('/');
+  click('.new-reminder--button');
+  fillIn('.spec-input-title', 'brush teeth');
+  fillIn('.spec-input-date', "2016-10-20");
+  fillIn('.spec-input-notes', 'also floss, probably should do this more often');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/new');
+    assert.equal(find('.spec-input-title').val(), "brush teeth");
+    assert.equal(find('.spec-input-date').val(), "2016-10-20");
+    assert.equal(find('.spec-input-notes').val(), "also floss, probably should do this more often");
+  });
+
+  click('.new-reminder--submit');
+
+  andThen(function() {
+    assert.equal(Ember.$('.spec-reminder-item:last').text().trim(), 'brush teeth');
+    assert.equal(Ember.$('.list-date:last').text().trim(), 'Wed Oct 19 2016 18:00:00 GMT-0600 (MDT)');
+  });
+});
--- tests/unit/controllers/reminders-test.js
+++ tests/unit/controllers/reminders-test.js
@@ -1,11 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('route:new-reminder', 'Unit | Route | new reminder', {
+moduleFor('controller:reminders', 'Unit | Controller | reminders', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
+// Replace this with your real tests.
 test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+  let controller = this.subject();
+  assert.ok(controller);
 });
