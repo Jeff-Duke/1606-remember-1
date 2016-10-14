cff03952bbcf8016746763aa8827d4790b07db3a
David Kerr <kerrd89@gmail.com>
2016-10-14 14:58:45 -0600

    recommitting to see if github updates

---
+++ app/controllers/reminders/reminder.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    editReminder: function() {
+      this.toggleProperty('isEditing');
+    },
+
+    saveReminder(reminderForm) {
+      let reminder = reminderForm.getProperties('title', 'date', 'notes', 'id');
+      reminder.date = new Date(reminder.date);
+      this.get('store').findRecord('reminder', reminder.id).then(function(targetReminder){
+        targetReminder.setProperties({title: reminder.title, date: reminder.date, notes: reminder.notes}).save();
+      });
+      this.toggleProperty('isEditing');
+    }
+  }
+
+});
