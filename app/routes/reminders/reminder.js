99ebb27f159804694f2c23f5428f99a073a23314
Jeff Duke <jeff.duke@gmail.com>
2016-10-18 16:10:35 -0600

    refactor delete action

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -2,14 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    deleteReminder(reminderForm) {
-      let reminder = reminderForm.getProperties('id');
-      this.get('store').findRecord('reminder', reminder.id, {
-        backgroundReload: false
-      }).then(targetReminder => {
-        targetReminder.destroyRecord();
-      }).then(() => {this.transitionTo('reminders');});
+    deleteReminder(reminderForm){
+        reminderForm.destroyRecord().then(() => {this.transitionTo('reminders');
+      });
     },
+
     willTransition() {
       var reminder = this.controllerFor('reminders.reminder');
       reminder.set('isEditing', false);
