8c1211c06877ca67739f86d06f0f95a656adb93d
David Kerr <kerrd89@gmail.com>
2016-10-18 17:14:55 -0600

    search filter added

--- app/controllers/reminders.js
+++ app/controllers/reminders.js
@@ -1,5 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: ['search'],
+  search: null,
+  reminders: Ember.computed('search', function() {
+    const search = this.get('search');
+    let reminders = this.get('model');
+    if(search) {
+      reminders = reminders.filter(reminder => {
+        return reminder.get('title').toLowerCase().match(search);
+      });
+    }
+    return reminders;
+  }),
+
+  actions: {
+    updateSearch(search) {
+      this.set('search', search  && search.toLowerCase());
+      console.log(search);
+    }
+  }
 
 });
